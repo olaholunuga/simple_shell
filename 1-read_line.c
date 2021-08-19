85eced95049fa7f3f0e54907e405f3e28ede8f51
Bakaryc50 <bakaryc50@gmail.com>
2021-08-19 08:10:20 -0700

    Bakary commit: simple_shell/0-av,1-read_line

--- 1-read_line.c
+++ 1-read_line.c
@@ -14,17 +14,21 @@ int main(void)
 	size_t bufsize_len = 0;
 	ssize_t nb_line_read;
 
+	/** prints $ and waits for a user **/
 	write(STDERR_FILENO, "$ ", 2);
 
+	/** reading the line **/
 	nb_line_read = getline(&line, &bufsize_len, stdin);
-	
+
+	/** checking th return value of the line **/
 	if (nb_line_read > 0)
 		write(STDOUT_FILENO, line, nb_line_read);
 	else
 		write(STDOUT_FILENO, "\n", 1);
-	
+
 	free(line);
 
+	/** if reading the line fails **/
 	if (nb_line_read == -1)
 		return (EXIT_FAILURE);
 
